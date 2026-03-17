f02b998dff2cdd93a64144bdad345eaef236b1b7
Amal-nellanhi <amalnellanhi@gmail.com>
2026-03-17 16:31:48 +0530

    Add script for index

---
+++ script.js
@@ -0,0 +1,37 @@
+// Add Task
+function addTask() {
+  let input = document.getElementById("taskInput");
+
+  if (input.value === "") return;
+
+  let li = document.createElement("li");
+  li.innerText = input.value;
+
+  document.getElementById("taskList").appendChild(li);
+
+  input.value = "";
+}
+
+// Dark Mode
+function toggleTheme() {
+  document.body.classList.toggle("dark");
+}
+
+// Timer
+let time = 1500;
+let interval;
+
+function startTimer() {
+  clearInterval(interval);
+
+  interval = setInterval(() => {
+    time--;
+
+    let min = Math.floor(time / 60);
+    let sec = time % 60;
+
+    document.getElementById("timer").innerText =
+      `${min}:${sec < 10 ? "0" : ""}${sec}`;
+
+  }, 1000);
+}
