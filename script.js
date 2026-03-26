940061127cd5bdcda56c2f060a0b4d5cb1a625dd
Harshit Gupta <153393197+Ashusf90@users.noreply.github.com>
2026-03-26 09:54:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,3 +35,29 @@ function startTimer() {
 
   }, 1000);
 }
+function startTimer() {
+  clearInterval(interval);
+
+  interval = setInterval(() => {
+    if (time <= 0) {
+      clearInterval(interval);
+      document.getElementById("timer").innerText = "00:00";
+
+      // play alarm (if you added earlier)
+      if (typeof alarmSound !== "undefined") {
+        alarmSound.play();
+      }
+
+      return;
+    }
+
+    time--;
+
+    let min = Math.floor(time / 60);
+    let sec = time % 60;
+
+    document.getElementById("timer").innerText =
+      `${min}:${sec < 10 ? "0" : ""}${sec}`;
+
+  }, 1000);
+}
