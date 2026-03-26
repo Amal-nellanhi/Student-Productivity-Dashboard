5ebcb83d26f098127f3cc4be5fdd406d6c3bf77e
Harshit Gupta <153393197+Ashusf90@users.noreply.github.com>
2026-03-26 09:35:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,18 @@ function addTask() {
 
   input.value = "";
 }
+window.onload = function () {
+  const greeting = document.getElementById("greeting");
+  const hour = new Date().getHours();
+
+  if (hour < 12) {
+    greeting.textContent = "Good Morning! ☀️";
+  } else if (hour < 18) {
+    greeting.textContent = "Good Afternoon! 🌤️";
+  } else {
+    greeting.textContent = "Good Evening! 🌙";
+  }
+};
 
 // Dark Mode
 function toggleTheme() {
